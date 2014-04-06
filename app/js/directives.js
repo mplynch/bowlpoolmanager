8ac56b3d245a7289e3bc5a9bc525416b8e91e90d
Michael Lynch <mplynch@gmail.com>
2014-04-05 23:10:13 -0500

    Implemented bpm-show-admin directive
    Implemented bpm-show-admin directive and applied it to the admin menu.
    Adjusted security rules to allow reading of the admin list by all
    authorized users.
    
    Closes #22

--- app/js/directives.js
+++ app/js/directives.js
@@ -2,10 +2,31 @@
 
 /* Directives */
 
-
 angular.module('myApp.directives', []).
-  directive('appVersion', ['version', function(version) {
-    return function(scope, elm, attrs) {
-      elm.text(version);
-    };
-  }]);
+    directive('appVersion', ['version', function (version) {
+        return function (scope, elm, attrs) {
+            elm.text(version);
+        };
+
+    }])
+
+    .directive('bpmShowAdmin', function ($rootScope, syncData) {
+        return {
+            restrict: 'A',
+            compile: function (el, attr) {
+                $rootScope.$on("$firebaseSimpleLogin:login", function () {
+                    console.log('checking for admin rights');
+                    var admins = syncData('admins');
+
+                    admins.$on("loaded", function() {
+                        var isAdmin = $rootScope.auth.user.uid in admins;
+                        el.toggleClass('hide', !isAdmin);
+                    });
+                });
+
+                $rootScope.$on("$firebaseSimpleLogin:logout", function () {
+                    el.toggleClass('hide', true);
+                });
+            }
+        }
+    });
